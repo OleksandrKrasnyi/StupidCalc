579124305ca6a76d3055124478ac6a141656ceb4
OleksandrKrasnyi <oskrasnyi@gmail.com>
2021-03-11 10:41:16 +0200

    Add Calc v.0.0.01

---
+++ CalcOutput/src/main/java/Printer.java
@@ -0,0 +1,11 @@
+import sanya.calc.Calculator;
+
+public class Printer {
+
+    public static void main(String[] args) {
+
+        int result = Calculator.add(5,7);
+
+        System.out.println(result);
+    }
+}
